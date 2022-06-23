9ff3d3a0e7955d625d255c87d1553609fc815fdd
pedermae <pedermae@stud.ntnu.no>
2022-06-23 15:56:52 +0200

    Added extensions.cs

---
+++ NorbitsChallenge/Dal/Extensions.cs
@@ -0,0 +1,23 @@
+﻿
+using NorbitsChallenge.Models;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace NorbitsChallenge.Dal
+{
+    public static class ParameterClass
+    {
+        public static SqlParameterCollection AddToTable(this SqlParameterCollection parameter, string Column, string Value)
+        {
+            parameter.Add($"@{Column}", System.Data.SqlDbType.NVarChar);
+            parameter[$"@{Column}"].Value = Value;
+            return parameter;
+        }
+        public static SqlParameterCollection AddToTable(this SqlParameterCollection parameter, string Column, int Value)
+        {
+            parameter.Add($"@{Column}", System.Data.SqlDbType.Int);
+            parameter[$"@{Column}"].Value = Value;
+            return parameter;
+        }
+    }
+}
